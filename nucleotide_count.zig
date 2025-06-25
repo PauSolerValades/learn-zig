485192ae80846c7a5ed2cddc3df189253d28d67a
PauSolerValades <pau.soler.valades@gmail.com>
2025-06-25 12:11:49 +0200

    Fet també amb hashmap per si acàs

--- nucleotide_count.zig
+++ nucleotide_count.zig
@@ -7,10 +7,14 @@ const ADNError = error{
 };
  
 pub fn main() !void {
-	const chain: []const u8 = "AAAACCCCGGGGTrTTT";
+	const chain: []const u8 = "AAAACCCCGGGGTTTT";
 	const resultat = try count_nucleotides(chain);
 	
 	print("{any} de cada\n", .{resultat});
+        
+        _ = try count_nucleotids_hm(chain);
+        
+        
 }
 
 fn count_nucleotides(comptime chain: []const u8) error{InvalidCharacter}![4]u32 {
@@ -35,6 +39,37 @@ fn count_nucleotides(comptime chain: []const u8) error{InvalidCharacter}![4]u32 
 	return counter;
 }
 
+fn count_nucleotids_hm(chain: []const u8) (error{InvalidCharacter} || error{OutOfMemory})!std.AutoHashMap(u8, u32) {
+    print("Entra {s}\n", .{chain});
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    
+    const allocator = arena.allocator();
+
+    var counter = std.AutoHashMap(u8, u32).init(allocator);
+    defer counter.deinit();
+
+    try counter.put('A', 0);
+    try counter.put('T', 0);
+    try counter.put('G', 0);
+    try counter.put('C', 0);
+
+    // convertim els strings en dos bits, ja que hi ha 4 grafies i 2^2 = 4 ergo u2
+    for (chain) |nuc| {
+         if (counter.getPtr(nuc)) |value_ptr| {
+            value_ptr.* += 1;
+        } else {
+            return error.InvalidCharacter;
+        }        
+    }
+    var iter = counter.iterator();
+    while (iter.next()) |entry| {
+        std.debug.print("  {c} => {d},\n", .{ entry.key_ptr.*, entry.value_ptr.* });
+    }
+
+    return counter;
+
+}
 test "valid characters" {
 	try testing.expectEqual([4]u32{4,4,4,4}, count_nucleotides("AAAACCCCTTTTGGGG"));
 }
